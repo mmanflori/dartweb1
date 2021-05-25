0a7ac39f95651e17287ab8c15a6d7070704ea0f4
Florian Brielmaier <mmanflori@gmail.com>
2021-05-25 23:44:26 +0200

    aenderung2

--- dartweb1/web/main.dart
+++ dartweb1/web/main.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
 
 void main() {
-  querySelector('[name="felix"]').text = 'Die Anwendung funktioniert !';
+  querySelector('[name="felix"]').text = 'Die Dart Anwendung funktioniert !';
   NumberInputElement meinWert = querySelector('#nummer');
   meinWert.value = '33';
 }
