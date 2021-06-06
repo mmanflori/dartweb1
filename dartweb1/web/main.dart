522e054e0d0f4d78fbfe4621e07f3565185c8192
Florian Brielmaier <mmanflori@gmail.com>
2021-06-06 18:05:39 +0200

    aktuelle version

--- dartweb1/web/main.dart
+++ dartweb1/web/main.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
 
 class Aufgabe {
-  static int _id = 0; //fix ID incrementation.
+  static int _id = 0;
   int id;
   final String text;
 
@@ -14,7 +14,7 @@ void aufgabeLoeschen(MouseEvent ereignis) {
   ereignis.stopPropagation();
   var div = (ereignis.currentTarget as Element).parent;
   var button = (ereignis.currentTarget as Element);
-  int schluessel = int.parse(button.id.split('-')[0]);
+  var schluessel = int.parse(button.id.split('-')[0]);
   aufgabenListe.removeWhere((aufgabe) => aufgabe.id == schluessel);
   div.remove();
 }
